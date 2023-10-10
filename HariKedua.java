61ad414e55f29d414726a8d43b414fbdcfcda02f
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-10-10 13:08:14 +0800

    Delete HariKedua.java
    Hapus

--- HariKedua.java
+++
@@ -1,8 +0,0 @@
-public class HariKedua {
-    public static void main(String[] args) {
-        int angka1 = 5;
-        int angka2 = 7;
-        int hasil = angka1 + angka2;
-        System.out.println("Hasil penjumlahan: " + hasil);
-    }
-}
