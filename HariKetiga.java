d0c2fcf2d8655599d525851d6ea75a66b2643854
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-10-11 19:18:41 +0800

    HariKetiga.java

---
+++ HariKetiga.java
@@ -0,0 +1,10 @@
+public class HariKetiga {
+    public static void main(String[] args) {
+        int angka = 10;
+        if (angka > 5) {
+            System.out.println("Angka lebih besar dari 5");
+        } else {
+            System.out.println("Angka tidak lebih besar dari 5");
+        }
+    }
+}
