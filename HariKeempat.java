328a60377337fc1c3043a3b81aab0c5ff21784ed
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-10-12 17:19:28 +0800

    HariKeempat

---
+++ HariKeempat.java
@@ -0,0 +1,8 @@
+public class HariKeempat {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+        for (int i : angka) {
+            System.out.println(i);
+        }
+    }
+}
