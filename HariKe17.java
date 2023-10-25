c20fe10eab8bd3c391ce527fde4c556ceecfd8f8
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-10-25 20:34:07 +0800

    Hari 17

---
+++ HariKe17.java
@@ -0,0 +1,10 @@
+package days100;
+
+public class HariKe17 {
+  public static void main(String[] args) {
+    String x = "20";
+    String y = "23";
+    String z = x + y;
+    System.out.println(z);
+  }
+}
