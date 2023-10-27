85a822c7963387784b2782e3c1d7c73eb677dc10
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-10-27 18:59:44 +0800

    Hari19

---
+++ HariKe19.java
@@ -0,0 +1,11 @@
+package days100;
+
+public class HariKe19 {
+  public static void main(String[] args) {
+    int i = 10;
+    while (i < 25) {
+      System.out.println(i);
+      i++;
+    }  
+  }
+}
