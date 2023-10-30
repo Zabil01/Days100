b8e196ec1b124d97c9a9527450f74630a6d34f2f
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-10-30 19:43:24 +0800

    HariKe22

---
+++ day22.java
@@ -0,0 +1,10 @@
+package days100;
+
+public class day22 {
+  public static void main(String[] args) {   
+    int time = 30;
+    String result;
+    result = (time < 40) ? "Good day." : "Good evening.";
+    System.out.println(result);
+  }
+}
