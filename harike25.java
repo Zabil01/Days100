3c157ac4d37f00d28a53c24385c7d0150c728ab3
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-02 18:19:11 +0800

    Operator aritmatika

---
+++ harike25.java
@@ -0,0 +1,29 @@
+package days100;
+
+public class harike25 {
+    public static void main(String[] args) {
+        // Mendefinisikan dua angka
+        int angka1 = 10;
+        int angka2 = 25;
+
+        // Operator Aritmatika: Penambahan
+        int hasilTambah = angka1 + angka2;
+        System.out.println("Hasil Penambahan: " + hasilTambah);
+
+        // Operator Aritmatika: Pengurangan
+        int hasilKurang = angka1 - angka2;
+        System.out.println("Hasil Pengurangan: " + hasilKurang);
+
+        // Operator Aritmatika: Perkalian
+        int hasilKali = angka1 * angka2;
+        System.out.println("Hasil Perkalian: " + hasilKali);
+
+        // Operator Aritmatika: Pembagian
+        int hasilBagi = angka1 / angka2;
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+
+        // Operator Aritmatika: Modulo (Sisa Bagi)
+        int sisaBagi = angka1 % angka2;
+        System.out.println("Sisa Bagi: " + sisaBagi);
+    }
+}
