a6d6af4a4a9bff250bce92ba2fc4e74adc5e071d
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-04 20:38:15 +0800

    Menghitung luas

---
+++ harike27.java
@@ -0,0 +1,16 @@
+package days100;
+
+public class harike27 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        double panjang = 7.0;
+        double lebar = 5.0;
+        double luas;
+
+        // Menghitung luas persegi panjang
+        luas = panjang * lebar;
+
+        // Menampilkan hasil
+        System.out.println("Luas persegi panjang dengan panjang " + panjang + " dan lebar " + lebar + " adalah " + luas);
+    }
+}
