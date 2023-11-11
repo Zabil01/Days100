cce3c393fec1214dd20ef35410f72bd5ccbde5d8
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-11 23:02:24 +0800

    Program for

---
+++ harike34.java
@@ -0,0 +1,19 @@
+package days100;
+
+public class harike34 {
+    public static void main(String[] args) {
+        // Menghitung jumlah bilangan bulat dari 1 hingga 10
+        System.out.println("Contoh: Menghitung jumlah bilangan bulat dari 1 hingga 10");
+
+        // Inisialisasi variabel jumlah dengan nilai awal 0
+        int jumlah = 0;
+
+        // Loop for untuk menghitung jumlah bilangan bulat
+        for (int i = 1; i <= 10; i++) {
+            jumlah += i; // Menambahkan nilai i ke variabel jumlah
+        }
+
+        // Menampilkan hasil
+        System.out.println("Jumlah: " + jumlah);
+    }
+}
