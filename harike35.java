8964e6da6b16fa584b065b5d3f58d401ab0846eb
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-12 23:17:09 +0800

    Program for2

---
+++ harike35.java
@@ -0,0 +1,18 @@
+package days100;
+
+public class harike35 {
+    public static void main(String[] args) {
+        
+        System.out.println("program Java dengan penggunaan loop for:");
+
+        
+        System.out.println("Menghitung hasil pangkat dua dari angka 5 hingga 10:");
+        for (int i = 5; i <= 10; i++) {
+            
+            int hasilPangkatDua = i * i;
+
+            
+            System.out.println("Angka " + i + " pangkat dua adalah " + hasilPangkatDua);
+        }
+    }
+}
