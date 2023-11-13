477f9d05d930c1f3d9f4b98d83be356df43dfaa9
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-14 00:02:10 +0800

    Operasi aritmatika

---
+++ harike36.java
@@ -0,0 +1,21 @@
+package days100;
+
+public class harike36 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah program Java operasi aritmatika");
+
+        // Mendeklarasikan dan menginisialisasi variabel
+        int angka1 = 10;
+        int angka2 = 10;
+
+        // Melakukan operasi aritmatika
+        int hasilPenjumlahan = angka1 + angka2;
+        int hasilPerkalian = angka1 * angka2;
+        
+
+        // Menampilkan hasil operasi aritmatika
+        System.out.println("Hasil penjumlahan " + angka1 + " + " + angka2 + " adalah " + hasilPenjumlahan);
+        System.out.println("Hasil perkalian " + angka1 + " + " + angka2 + " adalah " + hasilPerkalian);
+    }
+}
