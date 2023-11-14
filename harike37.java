108cbc9560f4df2d2447ac0420f6ae8b218f6290
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-14 23:06:12 +0800

    Operasi Aritmatika

---
+++ harike37.java
@@ -0,0 +1,27 @@
+package days100;
+
+public class harike37 {
+    public static void main(String[] args) {
+        
+        //Deklarasi variabel
+        int a = 10;
+        int b = 5;
+        int c;
+
+        // Penjumlahan
+        c = a + b;
+        System.out.println("Hasil penjumlahan: " + c);
+
+        // Pengurangan
+        c = a - b;
+        System.out.println("Hasil pengurangan: " + c);
+
+        // Perkalian
+        c = a * b;
+        System.out.println("Hasil perkalian: " + c);
+
+        // Pembagian
+        c = a / b;
+        System.out.println("Hasil pembagian: " + c);
+    }
+}
