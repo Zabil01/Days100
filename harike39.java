df3ccb512b52393ae429bee72783d52d1a20f520
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-16 18:49:07 +0800

    Penggabungan nama depan dan nama belakang

---
+++ harike39.java
@@ -0,0 +1,12 @@
+package days100;
+
+public class harike39 {
+    public static void main(String[] args) {
+        // Memasukkan nama depan dan nama belakang
+        String namaDepan = "Sabil";
+        String namaBelakang = "Muhammad";
+
+        // Menampilkan hasil
+        System.out.println("Hasil: " + namaBelakang + " " + namaDepan);
+    }
+}
