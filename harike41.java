d975425892397ac75086614d75153e04c31489ce
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-19 22:43:04 +0800

    Pola segitiga dengan kotak di setiap baris

---
+++ harike41.java
@@ -0,0 +1,34 @@
+package days100;
+
+import java.util.Scanner;
+
+public class harike41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("masukkan nilai n : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("+");
+            for (int j = 0; j < i; j++) {
+                System.out.print("---+");
+            }
+            
+            System.out.println();
+            
+            System.out.print("|");
+            for (int j = 0; j < i; j++) {
+                System.out.print("   |");
+            }
+            
+            System.out.println();
+            
+        }
+        System.out.print("+");
+        for (int i = 0; i < n; i++) {
+            System.out.print("---+");
+        }
+        System.out.println();
+    }
+}
