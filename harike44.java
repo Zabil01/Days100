200db9ce049518afadbf23ab7d1e099acdaaf26e
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-21 18:44:16 +0800

    Perulangan membuat bintang

---
+++ harike44.java
@@ -0,0 +1,11 @@
+package days100;
+
+public class harike44 {
+    public static void main(String[] args){
+
+        for(int i=0; i <= 10; i++){
+            System.out.println("*****");
+        }
+
+    }
+}
