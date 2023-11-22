a63c58d5d3f84f854889e7f457675e8675b76961
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-22 23:18:30 +0800

    Perulangan menggunakan do while

---
+++ harike45.java
@@ -0,0 +1,15 @@
+package days100;
+
+public class harike45 {
+    public static void main(String[] args) {
+
+        // membuat variabel
+        int i = 0;
+
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+
+    }
+}
