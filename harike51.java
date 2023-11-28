d879ec35823a1109109694fd2639117cd857745f
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-11-28 22:07:14 +0800

    Cek kelulusan menggunakan scanner

---
+++ harike51.java
@@ -0,0 +1,29 @@
+package days100;
+
+import java.util.Scanner;
+
+public class harike51 {
+
+    public static void main(String[] args) {
+
+        // membuat variabel dan Scanner
+        int nilai;
+        String nama;
+        Scanner scanner = new Scanner(System.in);
+
+        // mengambil input
+        System.out.print("Nama: ");
+        nama = scanner.nextLine();
+        System.out.print("Nilai: ");
+        nilai = scanner.nextInt();
+
+        // cek apakah dia lulus atau tidak
+        if( nilai >= 50 ) {
+            System.out.println("Selemat " + nama + ", anda lulus!");
+        } else {
+            System.out.println("Maaf " + nama + ", anda gagal");
+        }
+
+    }
+
+}
