8b672688f227320eb7ff266b1123f8d887821315
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-12-06 22:20:40 +0800

    Perulangan menggunakan for

---
+++ harike59.java
@@ -0,0 +1,10 @@
+package days100;
+
+public class harike59 {
+   public static void main(String args[]){
+       int x;
+       for(x=1;x<10;x++){
+           System.out.println("Nilai X "+x);
+       }
+   }
+}
