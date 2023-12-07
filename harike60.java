b105634b5633ff7423be575bc66d237a4bd930f9
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-12-07 22:46:03 +0800

    Operator logika OR

---
+++ harike60.java
@@ -0,0 +1,12 @@
+package days100;
+
+public class harike60 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        boolean result = a || b;
+
+        System.out.println("a || b: " + result);
+    }
+}
