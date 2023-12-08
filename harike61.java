c8fb712c0017865a0c4b22297aeac965b19675f2
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-12-08 22:27:16 +0800

    Program switch- case

---
+++ harike61.java
@@ -0,0 +1,37 @@
+package days100;
+
+public class harike61 {
+    public static void main(String[] args) {
+        int day = 5;
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Senin";
+                break;
+            case 2:
+                dayName = "Selasa";
+                break;
+            case 3:
+                dayName = "Rabu";
+                break;
+            case 4:
+                dayName = "Kamis";
+                break;
+            case 5:
+                dayName = "Jumat";
+                break;
+            case 6:
+                dayName = "Sabtu";
+                break;
+            case 7:
+                dayName = "Minggu";
+                break;
+            default:
+                dayName = "Hari tidak valid";
+                break;
+        }
+
+        System.out.println("Hari ke-" + day + " adalah " + dayName);
+    }
+}
