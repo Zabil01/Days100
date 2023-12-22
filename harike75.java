88893e5836170ee1a03ad09c24bc4c2231dec75d
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-12-22 23:22:34 +0800

    Program mendefinisikan perkalian dua bilangan desimal

---
+++ harike75.java
@@ -0,0 +1,16 @@
+package days100;
+
+public class harike75 {
+    public static void main(String[] args) {
+        // Memanggil fungsi 
+        double hasil = kali(10.0, 20.0);
+
+        // Menampilkan hasil perkalian ke layar
+        System.out.println("Hasil perkalian: " + hasil);
+    }
+
+    // Mendefinisikan fungsi "kali" yang 
+    static double kali(double a, double b) {
+        return a * b; 
+    }
+}
