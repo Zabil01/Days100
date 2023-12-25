c4c57d6aee537277ca5e548cde18c34eccd735cc
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-12-25 23:10:52 +0800

    Menghitung dan menampilkan akar kuadrat

---
+++ harike78.java
@@ -0,0 +1,15 @@
+package days100;
+
+public class harike78{
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        float x = 16;
+
+        // Hitung akar kuadrat
+        float akar = (float) Math.sqrt(x);
+
+        // Tampilkan akar kuadrat
+        System.out.println("Akar kuadrat dari " + x + " adalah " + akar);
+    }
+}
