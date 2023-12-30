0649b08ed291628d8bf6a195d37f02db4ead9a1b
Zabil <99641448+Zabil01@users.noreply.github.com>
2023-12-30 23:21:23 +0800

    Menghitung dan menampilkan volume prisma segitiga

---
+++ harike83.java
@@ -0,0 +1,17 @@
+package days100;
+
+public class harike83 {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        float alas = 5;
+        float tinggi = 7;
+        float panjang = 3;
+
+        // Hitung volume
+        float volume = (alas * tinggi * panjang) / 2;
+
+        // Tampilkan volume
+        System.out.println("Volume prisma dengan alas 5, tinggi 7, dan panjang 3 adalah " + volume);
+    }
+}
