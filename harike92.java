78859506889cc5506f513f793b9d8581288d844e
Zabil <99641448+Zabil01@users.noreply.github.com>
2024-01-08 21:55:44 +0800

    Program meminta pengguna untuk memasukkan suatu angka dan angka tersebut akan habis dibagi tiga

---
+++ harike92.java
@@ -0,0 +1,22 @@
+package days100;
+
+import java.util.Scanner;
+
+public class harike92 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);  
+        System.out.print("Masukkan Angka : ");
+        int angka = sc.nextInt();
+        
+        System.out.println("Angka Yang Habis Di bagi Tiga : ");
+        boolean angka1 = false;
+        for (int i = 1; i <= angka; i++) {
+            if (i %3==0) {
+                System.out.println(i);
+                angka1 = true;
+            }}
+        if (! angka1) {
+            System.out.println("Kosong");
+        }
+    }
+}
