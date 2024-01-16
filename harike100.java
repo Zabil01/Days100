eb4ee40e51893e33a2c5def104b9860937186f32
Zabil <99641448+Zabil01@users.noreply.github.com>
2024-01-16 22:42:09 +0800

    Harike100
    Program meminta pengguna untuk memasukkan angka, dan kemudian menampilkan pola angka yang berurutan secara terbalik

---
+++ harike100.java
@@ -0,0 +1,24 @@
+package days100;
+
+import java.util.Scanner;
+
+public class harike100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan Angka  : ");
+        int angka = sc.nextInt();
+        
+        for (int i = angka; i >= 1 ; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= angka; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+   }
+}
